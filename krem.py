87dd1444d5e627ca730dc96425ca1c5589dc2611
John <filimanistr@gmail.com>
2021-07-16 04:41:02 +0700

    some changes

--- krem.py
+++ krem.py
@@ -11,47 +11,48 @@ import lang
 token = os.getenv('TOKEN')
 name = 0
 
-def khelp(peer_id, random_id):
-    """ SEND ALL THE COMMANDS WHICH BOT CAN GET """
-    message = '''Крем, Functions:\n
-/ Get help
-krem help\n
-/ Get the meaning of a word
-krem m <eng word>
-krem meaning <eng word>\n
-/ Get the full meaning with pronunciation (other definitions)
-krem fm <eng word>\n
-/ Get the translate of a sentence/word
-krem t <eng/rus sentence>
-krem т <eng/rus sentence>
-krem translate <eng/rus sentence>\n
-/ Get the pronunciation or the text
-krem say <eng word/sentence>\n
-// Get the chinese to russian translate and back
-krem fig <chinese/rus word/sentence>
-krem рис <chinese/rus word/senten'''
-    requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(peer_id, random_id, message, token))
 
-def ktranslate(peer_id, random_id, text):
-    """ TRANSLATE RUSSIAN TO ENGLISH AND BACK """
-    text = text[2]
+class Krem(vk.vkapi):
+    def __init__(self, peer_id, random_id):
+        # self.text = text[
+        self.peer_id = peer_id
+        self.random_id = random_id
 
-    language = lang.language(text)
-    response = language.translate()
+    def give_help(self):
+        """ SEND ALL THE COMMANDS WHICH BOT CAN GET """
+        message = '''Крем, Functions:\n
+    / Get help
+    krem help\n
+    / Get the meaning of a word
+    krem m <eng word>
+    krem meaning <eng word>\n
+    / Get the full meaning with pronunciation (other definitions)
+    krem fm <eng word>\n
+    / Get the translate of a sentence/word
+    krem t <eng/rus sentence>
+    krem т <eng/rus sentence>
+    krem translate <eng/rus sentence>\n
+    / Get the pronunciation or the text
+    krem say <eng word/sentence>\n
+    // Get the chinese to russian translate and back
+    krem fig <chinese/rus word/sentence>
+    krem рис <chinese/rus word/senten'''
+        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(self.peer_id, self.random_id, message, token))
 
-    message = response
-    requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(peer_id, random_id, message, token))
+    def give_meaning(self, word):
+        language = lang.language(word)
+        message = language.define()
 
-def kmeaning(peer_id, random_id, text):
-    """ SEND A DEFINITION OF WORD """
-    text = text[2]
+        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(self.peer_id, self.random_id, message, token))
 
-    language = lang.language(text)
-    response = language.define()
-    message = response
+    def give_full_meaning(self, text):
+        pass
 
-    requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(peer_id, random_id, message, token))
-
+    def give_translate(self, text):
+        language = lang.language(text)
+        message = language.translate()
+        
+        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(self.peer_id, self.random_id, message, token))
 
 def kfullmeaning(peer_id, random_id, text):
     """ SEND ALL THE DEFINITIONS OF WORD TO THE USER + prononciaton """
@@ -125,7 +126,7 @@ if __name__ == '__main__':
 
     while True:
         updates = vkapi.ListenLP()
-        # print(updates)
+        print(updates)
 
         if updates['type'] == 'message_new':
             text = updates['object']['message']['text']
@@ -133,8 +134,10 @@ if __name__ == '__main__':
             if len(text) > 1 and text[0] == 'krem' or text[0] == "Krem" or text[0] == "Крем" or text[0] == "крем":
                 peer_id = updates['object']['message']['peer_id']
                 random_id = updates['object']['message']['random_id']
+                krem = Krem(peer_id, random_id)
+                
                 if text[1] == 'help':
-                    khelp(peer_id, random_id)
+                    krem.give_help()
 
                 if text[1] == 'fig' or text[1] == 'рис':
                     try:
@@ -144,7 +147,8 @@ if __name__ == '__main__':
 
                 if text[1] == 't' or text[1] == 'т' or text[1] == 'translate':
                     try:
-                        ktranslate(peer_id, random_id, text)
+                        text = text[2]
+                        krem.give_translate(text)
                     except:
                         requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=Something worng, use only russian and englins languages&access_token=%s&v=5.131'%(peer_id, random_id, token))
 
@@ -162,8 +166,11 @@ if __name__ == '__main__':
 
                 if text[1] == 'm' or text[1] == 'meaning':
                     try:
-                        kmeaning(peer_id, random_id, text)
+                        word = text[2]
+                        krem.give_meaning(word)
                     except:
                         requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=Try another word&access_token=%s&v=5.131'%(peer_id, random_id, token))
-                from streamlit import caching
-                caching.clear_cache()
+                
+                # Clear cache after script
+                # from streamlit import caching
+                # caching.clear_cache()
