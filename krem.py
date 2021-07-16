0bf95939e43ba7663853900b2838e3962efeee54
John Brown <filimanistr@gmail.com>
2021-07-16 12:09:56 +0000

    structure changes

--- krem.py
+++ krem.py
@@ -2,6 +2,7 @@
 
 import os
 import json
+from queue import LifoQueue
 
 import requests
 
@@ -10,9 +11,9 @@ import lang
 
 token = os.getenv('TOKEN')
 name = 0
+stack = LifoQueue()
 
-
-class Krem(vk.vkapi):
+class Krem():
     def __init__(self, peer_id, random_id):
         # self.text = text[
         self.peer_id = peer_id
@@ -37,48 +38,41 @@ class Krem(vk.vkapi):
     // Get the chinese to russian translate and back
     krem fig <chinese/rus word/sentence>
     krem рис <chinese/rus word/senten'''
-        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(self.peer_id, self.random_id, message, token))
+        return message
 
     def give_meaning(self, word):
         language = lang.language(word)
         message = language.define()
+        return message
 
-        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(self.peer_id, self.random_id, message, token))
+    def give_full_meaning(self, text, vkapi):
+        """ SEND ALL THE DEFINITIONS OF WORD TO THE USER + prononciaton """
+        language = lang.language(text)
+        response = language.fdefine()
+        sound = language.pron()
+        message = response
 
-    def give_full_meaning(self, text):
-        pass
+        with open('sounds/%s.ogg'%(text), 'wb') as f:
+            f.write(sound)
+
+        # For answers check the https://vk.com/dev/upload_files_3 (12's checkpoint)
+        f = open('sounds/%s.ogg'%(text), 'rb')
+        data = {'file':f}
+
+        link = vkapi.get('docs.getMessagesUploadServer', peer_id=self.peer_id, type='audio_message')
+        link = link['response']['upload_url']
+        rrlink = requests.post(link, files = data).json()
+        file_ = rrlink['file']
+
+        rrlink = requests.get('https://api.vk.com/method/docs.save?file=%s&title=%s.ogg&access_token=%s&v=5.131'%(file_, text, token)).text
+        mmlink = json.loads(rrlink)
+        response = requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&attachment=doc%s_%s_%s&access_token=%s&v=5.131'%(self.peer_id, self.random_id, message, mmlink['response']['audio_message']['owner_id'], mmlink['response']['audio_message']['id'], mmlink['response']['audio_message']['access_key'], token))
+        f.close()
 
     def give_translate(self, text):
         language = lang.language(text)
         message = language.translate()
-        
-        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&access_token=%s&v=5.131'%(self.peer_id, self.random_id, message, token))
-
-def kfullmeaning(peer_id, random_id, text):
-    """ SEND ALL THE DEFINITIONS OF WORD TO THE USER + prononciaton """
-    text = text[2]
-    language = lang.language(text)
-    response = language.fdefine()
-    sound = language.pron()
-    message = response
-
-    with open('sounds/%s.ogg'%(text), 'wb') as f:
-        f.write(sound)
-
-    # For answers check the https://vk.com/dev/upload_files_3 (12's checkpoint)
-    f = open('sounds/%s.ogg'%(text), 'rb')
-    data = {'file':f}
-    rlink = requests.get('https://api.vk.com/method/docs.getMessagesUploadServer?peer_id=%s&type=audio_message&access_token=%s&v=5.131'%(peer_id, token)).text
-    mlink = json.loads(rlink)
-    mlink = mlink['response']['upload_url']
-
-    rrlink = requests.post(mlink, files = data).json()
-    file_ = rrlink['file']
-
-    rrlink = requests.get('https://api.vk.com/method/docs.save?file=%s&title=%s.ogg&access_token=%s&v=5.131'%(file_, text, token)).text
-    mmlink = json.loads(rrlink)
-    response = requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=%s&attachment=doc%s_%s_%s&access_token=%s&v=5.131'%(peer_id, random_id, message, mmlink['response']['audio_message']['owner_id'], mmlink['response']['audio_message']['id'], mmlink['response']['audio_message']['access_key'], token))
-    f.close()
+        return message
 
 def ksay (peer_id, random_id, text):
     """ COMBINE DIFFERENT AUDIO FILES INTO ONE AUDIO MESSAGE """
@@ -135,9 +129,10 @@ if __name__ == '__main__':
                 peer_id = updates['object']['message']['peer_id']
                 random_id = updates['object']['message']['random_id']
                 krem = Krem(peer_id, random_id)
-                
+
                 if text[1] == 'help':
-                    krem.give_help()
+                    message = krem.give_help()
+                    vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message=message)
 
                 if text[1] == 'fig' or text[1] == 'рис':
                     try:
@@ -148,29 +143,35 @@ if __name__ == '__main__':
                 if text[1] == 't' or text[1] == 'т' or text[1] == 'translate':
                     try:
                         text = text[2]
-                        krem.give_translate(text)
+                        message = krem.give_translate(text)
+                        vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message=message)
                     except:
-                        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=Something worng, use only russian and englins languages&access_token=%s&v=5.131'%(peer_id, random_id, token))
+                        message = 'Something worng, use only russian and englins languages'
+                        vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message=message)
 
                 if text[1] == 'fm':
                     try:
-                        kfullmeaning(peer_id, random_id, text)
+                        text = text[2]
+                        message = krem.give_full_meaning(text, vkapi)
+                        # vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message=message)
                     except:
-                        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=Try another word&access_token=%s&v=5.131'%(peer_id, random_id, token))
+                        vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message="Try another word")
 
                 if text[1] == 'say':
                     try:
                         ksay(peer_id, random_id, text)
                     except:
-                        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=Try another word&access_token=%s&v=5.131'%(peer_id, random_id, token))
+                        vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message="Try another word")
 
                 if text[1] == 'm' or text[1] == 'meaning':
                     try:
                         word = text[2]
-                        krem.give_meaning(word)
+                        # stack.put([peer_id, random_id, word])
+                        message = krem.give_meaning(word)
+                        vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message=message)
                     except:
-                        requests.get('https://api.vk.com/method/messages.send?peer_id=%s&random_id=%s&message=Try another word&access_token=%s&v=5.131'%(peer_id, random_id, token))
-                
+                        vkapi.get('messages.send', peer_id=peer_id, random_id=random_id, message="Try another word")
+
                 # Clear cache after script
                 # from streamlit import caching
                 # caching.clear_cache()
