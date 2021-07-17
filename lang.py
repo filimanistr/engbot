863cf82354709be406185a1a47659abd5763b5f8
John Brown <filimanistr@gmail.com>
2021-07-17 19:05:01 +0000

    fix requests to collins dictionary + giving synonyms

--- lang.py
+++ lang.py
@@ -17,7 +17,7 @@ class language:
 
     def translate(self):
         """ Translates text, returns string with it """
-        if language.lan['lang']  == 'en':
+        if language.lan['lang'] == 'en':
             response = language.translator.translate(self.text, dest='ru').__dict__()
             return "%s - %s"%(self.text, response['text'])
         if language.lan['lang'] == 'ru':
@@ -27,7 +27,7 @@ class language:
 
     def define(self):
         """ Parses the first definition from Collins dictionary, and returns string with it """
-        if language.lan['lang']  == 'en':
+        if language.lan['lang'] == 'en':
             site = 'https://www.collinsdictionary.com/dictionary/english/%s'%(self.text)
             headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
             # Downoad html page
@@ -38,13 +38,21 @@ class language:
 
             word_type = soup.find('span', class_='gramGrp pos').text
             definition = soup.find('div', class_='def').text
-            response = '%s (%s) - %s'%(self.text, word_type, definition)
+            syn = soup.find('div', class_='thes')
+            s = []
+            if s != None:
+                for synonym in syn.find_all('a', class_='form ref'):
+                    s.append(synonym.text)
+
+            syn = ', '.join(s)
+            synonims = 'Synonyms: %s'%(syn)
+            response = '%s (%s) - %s%s'%(self.text.capitalize(), word_type, definition, synonims)
             return response
         return "Use an english words only"
 
     def fdefine(self):
         """ Parses all definitions from Collins dictionary, and returns string with all of them """
-        if language.lan['lang']  == 'en':
+        if language.lan['lang'] == 'en':
             site = 'https://www.collinsdictionary.com/dictionary/english/%s'%(self.text)
             headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
             # Downoad html page
@@ -55,27 +63,43 @@ class language:
 
             # Element with all definitions
             alld = soup.find('div', class_="content definitions cobuild br")
+            # list of certain blocks with definition
+            ad = []
+            for block in alld.find_all('div', class_='hom'):
+                ad.append(block)
 
-            # All grammar forms
-            gp = []
-            for response in alld.find_all('span', class_=['gramGrp pos', 'gramGrp']):
-                gp.append(response.text)
+            gp = [] # All grammar pforms
+            d = [] # All definitions
+            s = [] # All synonyms
+            for i in range(len(ad)):
+                phorm = ad[i].find('span', class_=['gramGrp pos', 'gramGrp'])
+                if phorm != None:
+                    gp.append(phorm.text)
 
-            # All definitions
-            d = []
-            for response in alld.find_all('div', class_='def'):
-                d.append(response.text)
+                defin = ad[i].find('div', class_='def')
+                if defin != None:
+                    d.append(defin.text)
+
+                s.append([])
+                syn = ad[i].find('div', class_='thes')
+                if syn != None:
+                    for synonym in syn.find_all('a', class_='form ref'):
+                        s[i].append(synonym.text)
 
             response = '%s\n\n'%(self.text.capitalize())
             for i in range(len(gp)):
-                response+='%s. %s\n%s\n'%(i+1, gp[i].capitalize(), d[i].capitalize())
+                response+='%s. %s\n%s'%(i+1, gp[i].capitalize(), d[i].capitalize())
+                if s != None and s[i] != []:
+                    syn = ', '.join(s[i])
+                    response+='Synonyms: %s'%(syn)
+                response+='\n\n'
 
             return response.rstrip()
         return "Use an english words only"
 
     def pron(self):
         """ Downloading mp3 pronunciation of word, and returns bytes of it """
-        if language.lan['lang']  == 'en':
+        if language.lan['lang'] == 'en':
             site = 'https://www.collinsdictionary.com/dictionary/english/%s'%(self.text)
             headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
             # Downoad html page
