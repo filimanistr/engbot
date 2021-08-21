95db5360b4cd7803b3d5211ee5a3ab61d8a96f4c
Dracula <vlad10game@gmail.com>
2021-08-21 11:38:12 +0300

    Refactoring code

--- vk.py
+++ vk.py
@@ -1,31 +1,26 @@
-import requests, json
+import requests
 
 class vkapi:
     def __init__(self, token):
         self.token = token
         self.data = None
+        self.update = {'v':'5.131', 'access_token': self.token}
 
-    def get(self, method, **kwargs):
-        param = {'v':'5.131', 'access_token':self.token}
-        param.update(kwargs)
-        r = requests.post('https://api.vk.com/method/'+method, data=param)
-        return json.loads(r.text)
+    def get(self, method, params, paramsUp=1):
+        if paramsUp:
+            params.update(self.update)
+
+        return requests.get(f"https://api.vk.com/method/{method}", params=params).json()
 
     def GetLP(self):
-        r = requests.post('https://api.vk.com/method/groups.getLongPollServer?group_id=202800459&access_token=%s&v=5.131'%(self.token))
-        self.data = json.loads(r.text)
+        self.data = requests.get("https://api.vk.com/method/groups.getLongPollServer", params={'group_id': '178007875', 'access_token': self.token, 'v': '5.131'}).json()
 
     def ListenLP(self):
-        try:
-            updates = requests.post('%s?act=a_check&key=%s&ts=%s&wait=25'%(self.data['response']['server'], self.data['response']['key'], self.data['response']['ts'])).json()
+       events = requests.get(self.data['response']['server'], params={'act': 'a_check', 'ts': self.data['response']['ts'], 'key': self.data['response']['key'], 'wait': 40}).json()
+       
+       if 'failed' in events:
+        self.GetLP()
+        return self.ListenLP()
 
-            if 'failed' in updates:
-                self.GetLP()
-                return self.ListenLP()
-
-            self.data['response']['ts'] = updates['ts']
-            return updates['updates']
-
-        except:
-            self.GetLP()
-            return self.ListenLP()
+       self.data['response']['ts'] = events['ts']
+       return events['updates']
