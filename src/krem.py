ccebd7cb080fc31fcf00680d5906f6b561901476
John <filimanistr@gmail.com>
2022-09-04 15:47:07 +0400

    feat: curse an image

--- src/krem.py
+++ src/krem.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # ---Dmitrij edit---
-# Python 3.7.3
+# Python 3.10.5
 
 import asyncio
 import aiohttp
@@ -21,7 +21,10 @@ dictionary = lang.Dictionary() # Empty
 
 bot = Bot(token, id=ID, is_group=True)
 dp = Dispatcher(bot)
+
 prefixes = ['krem', 'крем']
+connor = ['канар', 'каннар', 'конор', 'коннор', 'конар', 'коннар', 'connor']
+split = [*prefixes, *connor]
 
 @dp.message_handler(commands='help', prefixes=prefixes, ignore_case=True)
 async def help(message):
@@ -37,6 +40,11 @@ async def define(message):
     # Если это команда d ИЛИ команды нет вообще то ОК
     if message.get_command() not in commands:
         text = 'Unknown command'
+        await message.reply(text)
+        return True
+
+    if message.get_prefix() not in prefixes:
+        text = "unknown command"
         await message.reply(text)
         return True
 
@@ -97,10 +105,56 @@ async def translated(message):
     else: response = await dictionary.translate(text, 'de')
     await message.answer(response)
 
+import json
+from ctypes import *
+from PIL import Image
+import numpy as np
+
+cair = CDLL('./liball.so')
+cair.main.restype = None
+cair.main.argtypes = c_int,POINTER(c_char_p)
+
+def to_bitmap(image):
+    img = Image.open(image)
+    img.save('img/in.bmp')
+
+@dp.message_handler(commands=['кас', 'cas'], prefixes=split, ignore_case=True)
+async def pie(message):
+    global session
+    url = message.event['object']['message']['attachments'][0]['photo']['sizes'][-1]['url']
+    async with session.get(url) as ret:
+        html = await ret.read()
+
+    f = open('img/in.jpg', 'wb')
+    f.write(html)
+    f.close()
+
+    to_bitmap('img/in.jpg')
+    args =(c_char_p * 14)(b'-C', b'3', b'-T', b'1', b'-E', b'1', b'-I', b'img/in.bmp', b'-P', b'50', b'-R', b'0', b'-O', b'img/out.jpg' )
+    cair.main(len(args),args)
+
+    f = open('img/out.jpg', 'rb')
+    url = await bot.photos.getMessagesUploadServer(peer_id=message.peer_id)
+    async with session.post(url['response']['upload_url'], data={'photo':f}) as ret:
+        html = await ret.read()
+
+    f.close()
+    html = json.loads(html)
+    url = await bot.photos.saveMessagesPhoto(photo = html['photo'],
+                                            server = html['server'],
+                                            hash=html['hash'])
+    attachment = "photo%s_%s"%(url['response'][0]['owner_id'],
+                                url['response'][0]['id'])
+    await message.answer(message="Ваша пикча: ", attachment=attachment)
+
 dp.register_message_handler(define, chat_type='private')
 
 
 async def main():
+    global session
+    s = lang.Scrapper()
+    session = await s.get_session()
+
     await dictionary.write() # Fill it
     await dp.start_polling()
     await dictionary.destroy()
